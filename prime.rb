8825da86a09cdb7ed96a3a875a70942cdaad9de5
Michael Kim <m.kim4247@gmail.com>
2018-09-28 15:30:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(integer)
+  flag = true 
+  x = 2 
+  
+  if integer <= 1 
+    flag = false 
+  end 
+  
+  while x <= (integer / 2)
+    if integer % x == 0
+      flag = false 
+    end 
+    x += 1 
+  end 
+  
+  flag 
+end 
