3903c412f93521bafba31797abecaa6265abc3d9
Michael Kim <m.kim4247@gmail.com>
2020-10-07 16:17:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,27 +2,10 @@
 # if int is less than 1, return false
 
 def prime?(int)
-  if int <= 3
-    
+  if int <= 1
+    false
   elsif int % 2 == 0 || n % 3 == 0
     true
-  
-  i = 5
-  
   end
 end
 
-function is_prime(n)
-    if n ≤ 3 then
-        return n > 1
-    else if n mod 2 = 0 or n mod 3 = 0
-        return false
-
-    let i ← 5
-
-    while i × i ≤ n do
-        if n mod i = 0 or n mod (i + 2) = 0
-            return false
-        i ← i + 6
-
-    return true
