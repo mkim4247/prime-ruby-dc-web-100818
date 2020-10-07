406f5cb22202bc3fa7ac724d85dc2015da746c7d
Michael Kim <m.kim4247@gmail.com>
2020-10-07 15:29:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,5 @@
 # Add  code here!
 
-def prime?(integer)
-  flag = true 
-  x = 2 
+def prime?(int)
   
-  if integer <= 1 
-    flag = false 
-  end 
-  
-  while x <= (integer / 2)
-    if integer % x == 0
-      flag = false 
-    end 
-    x += 1 
-  end 
-  
-  flag 
-end 
+end
